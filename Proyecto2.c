331daf853d4c1ee01d7b80549728113a0d6df3c6
idaliendres <134539635+idaliendres@users.noreply.github.com>
2024-01-17 21:51:34 -0400

    Update Proyecto2.c
    Actualización con parche

--- Proyecto2.c
+++ Proyecto2.c
@@ -6,10 +6,11 @@
 
 typedef struct {
     int distance;
-    int hours;
-    // Otros campos como el costo si es necesario
+    int hours; // Como tenemos la conversión realmente no es necesario guardarlo, solo es para cambios futuros.
 } Route;
 
+
+// Inicializar matriz de grafos
 void initializeGraph(Route graph[MAX_LOCATIONS][MAX_LOCATIONS]) {
     for (int i = 0; i < MAX_LOCATIONS; i++) {
         for (int j = 0; j < MAX_LOCATIONS; j++) {
@@ -19,6 +20,7 @@ void initializeGraph(Route graph[MAX_LOCATIONS][MAX_LOCATIONS]) {
     }
 }
 
+// Cargar rutas del archivo
 void loadRoutes(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], const char* filename) {
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
@@ -43,6 +45,7 @@ void loadRoutes(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], const char* filename)
     fclose(file);
 }
 
+// Función dijkstra solicitada:
 void dijkstra(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], int source, int distances[MAX_LOCATIONS]) {
     bool visited[MAX_LOCATIONS] = {false};
 
@@ -71,24 +74,31 @@ void dijkstra(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], int source, int distanc
 }
 
 void showAllRoutesToDestination(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], char origin, char destination) {
+ 
+ 	// Inicialización ASCII, se asume que el destino y el origen esta en un rango A-Z
     int originIndex = origin - 'A';
     int destIndex = destination - 'A';
-
+	
     if (graph[originIndex][destIndex].distance == INT_MAX) {
-        printf("No hay forma de llegar desde %c hasta %c\n", origin, destination);
-        return;
+        printf("\nNo hay forma de llegar al menos de manera directa desde %c hasta %c\n", origin, destination);
+        // el return acá hacía que no estrase en el bucle for para el caso de rutas indirectas.
     }
 
-    printf("Rutas desde %c hasta %c:\n", origin, destination);
+    printf("\nRutas desde %c hasta %c:\n", origin, destination);
     printf("Distancia\tTiempo (en horas)\n"); 
 
-    if (graph[originIndex][destIndex].distance == 0) {
-        printf("0\t\t%.2f horas\n", (float)graph[originIndex][destIndex].distance / 100); // Convertir metros a horas
-    } else {
+    if (graph[originIndex][destIndex].distance != 0) {
         // Mostrar ruta directa
-        printf("Directo: %d mts\t%.2f horas\n", graph[originIndex][destIndex].distance, (float)graph[originIndex][destIndex].distance / 100);
-
-        // Mostrar rutas intermedias
+        
+        if (graph[originIndex][destIndex].distance != 2147483647) {	
+        	printf("Directo: %d mts\t%.2f horas\n", graph[originIndex][destIndex].distance, (float)graph[originIndex][destIndex].distance / 100);
+		}
+		
+		else {
+			printf("No es posible hacer tal recorrido de manera directa\n");
+		}
+		
+        // Mostrar rutas intermedias con lógica del recorrido de la matriz
         for (int i = 0; i < MAX_LOCATIONS; i++) {
             if (i != originIndex && i != destIndex &&
                 graph[originIndex][i].distance != INT_MAX &&
@@ -102,31 +112,38 @@ void showAllRoutesToDestination(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], char 
 }
 
 void showAllRoutesFromOrigin(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], char origin) {
-    int originIndex = origin - 'A';
+    
+	// Inicialización ASCII, se asume que el destino y el origen esta en un rango A-Z
+	int originIndex = origin - 'A';
 
-    printf("Todas las rutas desde %c:\n", origin);
-    printf("Destino\t\tDistancia\tTiempo (en horas)\n");
+    printf("\nTodas las rutas desde %c:\n", origin);
 
-    for (int destIndex = 0; destIndex < MAX_LOCATIONS; destIndex++) {
-        if (destIndex != originIndex) {
-            showAllRoutesToDestination(graph, origin, destIndex + 'A');
+    for (int destIndex = 0; destIndex < MAX_LOCATIONS; destIndex++) { // Buscar hasta que se acaben las posibles opciones
+        if (destIndex != originIndex) { // Validación para que un nodo no se busque consigo mismo
+            showAllRoutesToDestination(graph, origin, destIndex + 'A'); // Ir alternando constantemente el nodo de destino
         }
     }
 }
 
 void showOptimalRoute(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], int distances[MAX_LOCATIONS], char origin, char destination) {
-    int originIndex = origin - 'A';
+    
+    // Inicialización ASCII, se asume que el destino y el origen esta en un rango A-Z
+	int originIndex = origin - 'A';
     int destIndex = destination - 'A';
-
+	
+	/* Nota del programador: Se asume que en este caso el valor se desproporciono por lo tanto es INT_MAX, si eso sucede no hay 
+	manera posible de llegar al destino */
+	
     if (distances[destIndex] == INT_MAX) {
         printf("No hay forma de llegar desde %c hasta %c\n", origin, destination);
         return;
     }
 
-    printf("Ruta más óptima desde %c hasta %c:\n", origin, destination);
+    printf("\nRuta más óptima desde %c hasta %c:\n", origin, destination);
     printf("Distancia\tTiempo (en horas)\n");
 
-    printf("%d\t\t%.2f horas\n", distances[destIndex], (float)graph[originIndex][destIndex].distance / 100);
+	// Se hace la conversión según las condiciones dadas
+    printf("%d\t\t%.2f horas\n", distances[destIndex], (float)distances[destIndex] / 100);
 }
 
 void showMenu(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], int distances[MAX_LOCATIONS]) {
@@ -166,49 +183,22 @@ void showMenu(Route graph[MAX_LOCATIONS][MAX_LOCATIONS], int distances[MAX_LOCAT
     } while (choice != 3);
 }
 
-// main de prueba
-
-/* int main() {
-    Route graph[MAX_LOCATIONS][MAX_LOCATIONS];
-    initializeGraph(graph);
-    printf("Tenga en cuenta que el valor 2147483647, indica que no hay forma de realizar tal recorrido\n");
-
-    loadRoutes(graph, "prueba.txt");
-
-    int sourceNode = 0;
-    int distances[MAX_LOCATIONS];
-    for (int i = 0; i < MAX_LOCATIONS; i++) {
-        distances[i] = INT_MAX;
-    }
-
-    dijkstra(graph, sourceNode, distances);
-
-	
-    printf("Distancias mínimas desde el nodo %c:\n", sourceNode + 'A');
-    for (int i = 0; i < MAX_LOCATIONS; i++) {
-        printf("Hasta el nodo %c: %d horas\n", i + 'A', graph[sourceNode][i].hours);
-    }
-    
-
-    char originNode = 'A';
-    char destinationNode = 'E';
-
-    showAllRoutesFromOrigin(graph, originNode);
-    showOptimalRoute(graph, distances, originNode, destinationNode);
-
-    return 0;
-} */
-
 /* NOTA: El Programa solo puede calcular todas las rutas indirectas entre un destino y un origen si en algún momento ese origen 
-estuvo relacionado directamente con el destino.
+estuvo relacionado directamente con el destino. Un ejemplo: Si A->D luego D->E, La ruta indirectacon D solo se calcula si 
+previamente A->E
 
 En tal caso (si tuvo la relación directa) solo es capaz de calcular de manera correcta la distancia (en el caso de que sea 
 indirecta sin tener relación directa), de ese Nodo a su Nodo destino (Ha de ser una falla relativamente fácil). */
 
+/* Ya el bug fue parcheado */
+
 int main() {
     Route graph[MAX_LOCATIONS][MAX_LOCATIONS];
     initializeGraph(graph);
-    printf("Tenga en cuenta que el valor 2147483647 indica que no hay forma de realizar tal recorrido\n");
+    
+    /* Notas del programador:
+    printf("Tenga en cuenta que el valor 2147483647 indica que no hay forma de realizar tal recorrido (distancias)\n");
+    printf("Tenga en cuenta que el valor 21474836.00 indica que no hay forma de realizar tal recorrido (horas)\n"); */
 
     loadRoutes(graph, "prueba.txt");
 
